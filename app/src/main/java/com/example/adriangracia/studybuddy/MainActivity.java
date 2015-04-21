e0e3f1f668cc793203e0ffe37414c59c51d40827
Jmbrace <jmbrace@ilstu.edu>
2015-04-20 20:34:36 -0500

    tester commit

--- app/src/main/java/com/example/adriangracia/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/adriangracia/studybuddy/MainActivity.java
@@ -17,7 +17,7 @@ import com.example.adriangracia.studybuddy.fragment.*;
 
 
 public class MainActivity extends singleFragmentActivity {
-
+   //Julian commit test
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
