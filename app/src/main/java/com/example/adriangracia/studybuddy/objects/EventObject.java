1bc6a4ac3b8f2a8660b2cefcf8d3b9fc9ab873f6
Jonathan Mitchell <jmitch0901@gmail.com>
2015-04-23 13:38:08 -0500

    Jon's Branch-Commit 3:Added Factory Instances for later use.

--- app/src/main/java/com/example/adriangracia/studybuddy/objects/EventObject.java
+++ app/src/main/java/com/example/adriangracia/studybuddy/objects/EventObject.java
@@ -7,14 +7,39 @@ import java.io.Serializable;
  */
 public class EventObject implements Serializable{
 
-    String title;
-    String location;
-    TimeObject to;
+    private String title;
+    private String location;
+    private TimeObject to;
 
     public EventObject(String title, String location, TimeObject to){
         this.title=title;
         this.location=location;
         this.to=to;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public TimeObject getTo() {
+        return to;
+    }
+
+    public void setTo(TimeObject to) {
+        this.to = to;
+    }
+
 
 }
