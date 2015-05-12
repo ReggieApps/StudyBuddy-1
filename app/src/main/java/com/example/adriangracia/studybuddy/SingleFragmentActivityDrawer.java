023ebefdccc0ee876589c89267e58111a5e8c50c
Robby <rgilbertpaul@gmail.com>
2015-05-12 13:21:18 -0600

    Robby's Commit 9: Clean up and sidebar added.

---
+++ app/src/main/java/com/example/adriangracia/studybuddy/SingleFragmentActivityDrawer.java
@@ -0,0 +1,59 @@
+package com.example.adriangracia.studybuddy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ListView;
+
+import com.example.adriangracia.studybuddy.R;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Robby on 5/12/2015.
+ */
+public abstract class SingleFragmentActivityDrawer extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_fragment_activity_with_drawer);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        listView = (ListView)findViewById(R.id.list_view_left_drawer);
+
+        Fragment aFrag = getSupportFragmentManager().findFragmentById(R.id.frame_layout_drawer_content_frame);
+
+        if(aFrag == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.frame_layout_drawer_content_frame, getFragment()).commit();
+        }
+
+
+
+        setUpDrawerToggles();
+    }
+
+
+    //Set up the menu bar icon eventually!
+    private void setUpDrawerToggles(){
+        //new ActionBarDrawerToggle(this,drawerLayout,android.R.drawable.dra)
+    }
+
+
+    public DrawerLayout getDrawerLayout(){
+        return drawerLayout;
+    }
+
+    public ListView getDrawerListView(){
+        return listView;
+    }
+
+
+
+    public abstract Fragment getFragment();
+}
